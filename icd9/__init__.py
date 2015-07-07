f3c573efbb3756097f2a21fe9d98ddd906429713
Alex Kestner <alex.kestner@gmail.com>
2015-07-07 17:14:57 -0400

    stylistic update to icd9.__init__.py

--- icd9/__init__.py
+++ icd9/__init__.py
@@ -1,14 +1,7 @@
-from .initialize import (ahrqComorbidAll, elixComorbid, icd9Hierarchy,
-                         ahrqComorbid, icd9Chapters, quanElixComorbid)
-
-del initialize
-
-
-from .conversions import (decimal_to_parts, decimal_to_short,
-                          short_to_decimal, short_to_parts,
-                          parts_to_short, parts_to_decimal)
-
-del conversions
-
-
+from .initialize import (ahrqComorbidAll, elixComorbid, icd9Hierarchy, ahrqComorbid, icd9Chapters, quanElixComorbid)
+from .conversions import (decimal_to_parts, decimal_to_short, short_to_decimal, short_to_parts, parts_to_short, parts_to_decimal)
 from .counter import Counter
+
+__all__= ['ahrqComorbidAll', 'elixComorbid', 'icd9Hierarchy', 'ahrqComorbid', 'icd9Chapters', 'quanElixComorbid',
+          'decimal_to_parts', 'decimal_to_short', 'short_to_decimal', 'short_to_parts', 'parts_to_short', 'parts_to_decimal',
+          'Counter']
