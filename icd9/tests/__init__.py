dbe005851a74cb3dd181e966c4453b62f9d66403
Alex Kestner <alex.kestner@gmail.com>
2015-07-07 17:13:08 -0400

    converted tests from raw assert's to real unittest.TestCase's, created unittest.TestSuites's for icd9.conversions and added them to setup.test_suite

---
+++ icd9/tests/__init__.py
@@ -0,0 +1,5 @@
+from unittest import TestSuite
+from test_convert import all_convert_tests
+
+
+all_tests = TestSuite([all_convert_tests])
--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@ from setuptools import setup
 import os
 del os.link
 
-setup(name='icd9',
-            version='0.1',
-            description='Supports comorbidity analysis using ICD9 codes, based on the R package of the same name',
-            url='http://github.com/kshedden/icd9',
-            author='Kerby Shedden',
-            author_email='kshedden@umich.edu',
-            license='BSD',
-            packages=['icd9'],
-            include_package_data=True)
+setup(
+    name='icd9',
+    version='0.1',
+    description='Supports comorbidity analysis using ICD9 codes, based on the R package of the same name',
+    url='http://github.com/macroeyes/icd9',
+    author='Kerby Shedden',
+    author_email='kshedden@umich.edu',
+    license='BSD',
+    install_requires=[
+        'pandas',
+        'numpy'
+    ],
+    packages=['icd9'],
+    include_package_data=True,
+    test_suite ='icd9.tests.all_tests'
+)
